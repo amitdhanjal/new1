76febbe748a83fa761160d69049924d95886b8b1
Amit <amitdhanjal43@gmail.com>
2020-02-17 10:43:28 +0530

    application1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return "Hi,I am running on Host"
+	return "Hi,I am running on Host and testing the triggger"
 
 if __name__ == '__main__':
 	app.run(debug=True)
